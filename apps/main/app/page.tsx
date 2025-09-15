36af4f66acb958d0d44431694f7ed61763f4170d
MaxZhang <zsc.guru@gmail.com>
2025-09-15 15:22:28 +0800

    feat: 自我介绍的文字

--- apps/main/app/page.tsx
+++ apps/main/app/page.tsx
@@ -1,9 +1,10 @@
 import LightRays from '@/bits/LightRays/LightRays'
+import TextType from '@/bits/TextType/TextType'
 
 export default function Home() {
 	return (
 		<div className="">
-			<section className="relative">
+			<section className="relative -z-10">
 				<LightRays
 					raysOrigin="top-center"
 					raysColor="#00ffff"
@@ -15,6 +16,15 @@ export default function Home() {
 					noiseAmount={0.1}
 					distortion={0.05}
 					className="custom-rays"
+				/>
+			</section>
+			<section className="absolute top-1/5 left-1/2 transform -translate-x-1/2  text-[64px] font-bold ">
+				<TextType
+					text={['Hello, you!', '哈哈哈哈，我还没想好具体说啥', '先自己逛吧~']}
+					typingSpeed={75}
+					pauseDuration={1500}
+					showCursor={true}
+					cursorCharacter="_"
 				/>
 			</section>
 		</div>
