73468c05dd694fb7a521e7e30c34279f04c13cb0
MaxZhang <zsc.guru@gmail.com>
2025-09-25 10:32:21 +0800

    refactor: start vibe coding

--- apps/main/app/layout.tsx
+++ apps/main/app/layout.tsx
@@ -1,9 +1,6 @@
-import { Heart, House, Pen, User } from 'lucide-react'
 import type { Metadata } from 'next'
 import { ThemeProvider } from 'next-themes'
 
-import Dock from '@/bits/Dock/Dock'
-import Link from 'next/link'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -16,49 +13,12 @@ export default function RootLayout({
 }: Readonly<{
 	children: React.ReactNode
 }>) {
-	const items = [
-		{
-			icon: (
-				<Link href={'/'}>
-					<House />
-				</Link>
-			),
-			label: '首页',
-		},
-		{
-			icon: (
-				<Link href={'/post'}>
-					<Pen />
-				</Link>
-			),
-			label: '文字',
-		},
-		{
-			icon: (
-				<Link href={'/life'}>
-					<Heart />
-				</Link>
-			),
-			label: '生活',
-		},
-		{
-			icon: (
-				<Link href={'/me'}>
-					<User />
-				</Link>
-			),
-			label: '关于',
-		},
-	]
-
 	return (
 		<html lang="zh-CN" suppressHydrationWarning>
 			<body className="antialiased">
 				<ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
 					{children}
 				</ThemeProvider>
-
-				<Dock items={items} panelHeight={68} baseItemSize={50} magnification={70} />
 			</body>
 		</html>
 	)
--- apps/main/app/page.tsx
+++ apps/main/app/page.tsx
@@ -1,32 +1,3 @@
-import LightRays from '@/bits/LightRays/LightRays'
-import TextType from '@/bits/TextType/TextType'
-
 export default function Home() {
-	return (
-		<div className="">
-			<section className="relative -z-10">
-				<LightRays
-					raysOrigin="top-center"
-					raysColor="#00ffff"
-					raysSpeed={1.5}
-					lightSpread={0.8}
-					rayLength={1.2}
-					followMouse={true}
-					mouseInfluence={0.1}
-					noiseAmount={0.1}
-					distortion={0.05}
-					className="custom-rays"
-				/>
-			</section>
-			<section className="absolute top-1/5 left-1/2 transform -translate-x-1/2  text-[64px] font-bold ">
-				<TextType
-					text={['Hello, you!', '哈哈哈哈，我还没想好具体说啥', '先自己逛吧~']}
-					typingSpeed={75}
-					pauseDuration={1500}
-					showCursor={true}
-					cursorCharacter="_"
-				/>
-			</section>
-		</div>
-	)
+	return <div className="">home</div>
 }
